9d00150846e95ea11ea31fcee6b5d1d598bd0a98
Gabriel Silveira <ramos97033@outlook.com>
2025-03-03 15:16:49 -0300

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<Pessoa> pessoas = new List<Pessoa>{
+    new Pessoa("Gabriel", 27),
+    new Pessoa("Gabriela", 27)
+};
+
+foreach (var pessoa in pessoas){
+    Console.WriteLine($"{pessoa.Nome} tem {pessoa.Idade} anos");
+}
+
+public class Pessoa {
+    public string Nome {get; set;}
+    public int Idade { get; set;}
+    public Pessoa ( string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+};
